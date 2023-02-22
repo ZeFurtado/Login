01df6f27bafa765924e937ec8dcd69922533994b
Lukhas Furtado <lukhasfurtado9@gmail.com>
2023-02-22 02:22:55 -0300

    Classe com conexão e comandos MySql

---
+++ Login/MySqlOperations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace Login
+{
+    internal class MySqlOperations
+    {
+        DbData dadosDeAcesso = new DbData();
+
+        string DadosDeConexao() 
+        {
+            string conexao = $"SERVER={dadosDeAcesso.server};USER={dadosDeAcesso.user};PASSWORD={dadosDeAcesso.password};DATABASE={dadosDeAcesso.database}";
+            return conexao;
+        }
+
+    }
+}
