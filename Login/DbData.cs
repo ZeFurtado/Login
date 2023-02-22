84295b7d0ae19253aeb20ec2888e938cccf9dde3
Lukhas Furtado <lukhasfurtado9@gmail.com>
2023-02-22 02:20:39 -0300

    Classe com os dados de conexão e outra classe de operação dos comandos MySql

---
+++ Login/DbData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login
+{
+    internal class DbData
+    {
+        public string server = "";
+        public string user = "";
+        public string password = "";
+        public string database = "";
+    }
+}
